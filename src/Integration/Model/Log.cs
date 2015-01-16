77c3ef7ab90d3f62fbfebc2fab76619154c7f32a
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-01-16 14:26:53 +0100

    Razor Email templating

--- src/Integration/Model/Log.cs
+++ src/Integration/Model/Log.cs
@@ -7,14 +7,17 @@ namespace Vertica.Integration.Model
 	{
 		private readonly Action<string> _message;
 	    private readonly Action<Target, string> _warning;
+	    private readonly Action<Target, string> _error;
 
-	    internal Log(Action<string> message, Action<Target, string> warning)
+	    internal Log(Action<string> message, Action<Target, string> warning, Action<Target, string> error)
 		{
 			if (message == null) throw new ArgumentNullException("message");
 	        if (warning == null) throw new ArgumentNullException("warning");
+	        if (error == null) throw new ArgumentNullException("error");
 
 	        _message = message;
 	        _warning = warning;
+	        _error = error;
 		}
 
 		public void Message(string format, params object[] args)
@@ -25,6 +28,11 @@ namespace Vertica.Integration.Model
 	    public void Warning(Target target, string format, params object[] args)
 	    {
 	        _warning(target, String.Format(format, args));
+	    }
+
+	    public void Error(Target target, string format, params object[] args)
+	    {
+	        _error(target, String.Format(format, args));
 	    }
 	}
 }
