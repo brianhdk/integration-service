75d428bedf1421007d882183177870461834eea7
Henrik Rossen Jakobsen <henrik@rossenjakobsen.dk>
2015-01-16 16:03:31 +0100

    Worked on error listing

---
+++ src/Integration/Portal/Assets/js/app.js
@@ -0,0 +1,35 @@
+﻿var integrationsApp = angular.module('integrationsApp', ['ngRoute']);
+
+// configure our routes
+integrationsApp.config(function ($routeProvider) {
+    $routeProvider
+
+		// route for the home page
+		.when('/', {
+		    templateUrl: '/assets/pages/main.html',
+		    controller: 'mainController'
+		})
+
+        .when('/errors', {
+            templateUrl: '/assets/pages/errors.html',
+            controller: 'errorsController'
+        })
+
+        .when('/errors/:errorId', {
+            templateUrl: '/assets/pages/errors-detail.html',
+            controller: 'errorsController'
+        })
+
+		 // route for the about page
+        .when('/running-tasks', {
+            templateUrl: '/assets/pages/runningTasks.html',
+            controller: 'tasksController'
+        })
+
+		 // route for the about page
+        .when('/latest-tasks', {
+            templateUrl: '/assets/pages/latestTasks.html',
+            controller: 'tasksController'
+        })
+    ;
+});
---
+++ src/Integration/Portal/Assets/js/controller/mainController.js
@@ -0,0 +1,6 @@
+﻿integrationsApp.controller('mainController', function ($scope, $location) {
+    $scope.isActive = function (route) {
+        return route === $location.path();
+    }
+    $scope.message = 'main controller.';
+});
