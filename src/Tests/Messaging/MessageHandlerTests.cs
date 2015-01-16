af250d4b562aaa41383e1b18aae3ecbec6023937
Lasse Eskildsen <leskil99@gmail.com>
2015-01-16 11:26:03 +0100

    Refactoring of messaging project and test.

---
+++ src/Tests/Messaging/MessageHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Vertica.Integration.Tests.Messaging
+{
+	[TestFixture]
+	public class MessageHandlerTests
+	{
+		[Test]
+		public void CanHandle_ValidMessage_ReturnsTrue()
+		{
+			var subject = new MessageHandlerMock();
+			
+		}
+	}
+}
