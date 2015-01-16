775b3dc3d77bf337eb8c28025045dc1d458affc0
Lasse Eskildsen <leskil99@gmail.com>
2015-01-16 11:26:03 +0100

    Refactoring of messaging project and test.

---
+++ src/Tests/Messaging/MessageHandlerMock.cs
@@ -0,0 +1,17 @@
+﻿using Integration.Messaging;
+
+namespace Vertica.Integration.Tests.Messaging
+{
+	public class TestMessage
+	{
+		
+	}
+
+	public class MessageHandlerMock : MessageHandler<TestMessage>
+	{
+		protected override void HandleMessage(TestMessage message)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
