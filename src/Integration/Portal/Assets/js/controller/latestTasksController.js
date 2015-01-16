76845d5c413ec77d772b4975914e6d1fe7b577e5
Henrik Rossen Jakobsen <henrik@rossenjakobsen.dk>
2015-01-16 22:24:55 +0100

    Added paging and filtering

--- src/Integration/Portal/Assets/js/controller/latestTasksController.js
+++ src/Integration/Portal/Assets/js/controller/latestTasksController.js
@@ -1,6 +1,33 @@
-﻿integrationsApp.controller('latestTasksController', function ($scope, $http) {
+﻿integrationsApp.controller('latestTasksController', function ($scope, $http, $filter, ngTableParams) {
 
     $http.get("/latestTasks").success(function (xhr) {
-        $scope.tasks = xhr;
+        var data = xhr;
+
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 10,
+            sorting: {
+                //name: 'asc'     // initial sorting
+            },
+            filter: {
+                name: ''       // initial filter
+            }
+        }, {
+            getData: function ($defer, params) {
+                // use build-in angular filter
+                var filteredData = params.filter() ?
+                                   $filter('filter')(data, params.filter()) :
+                                   data;
+
+                var orderedData = params.sorting() ?
+                                    $filter('orderBy')(filteredData, params.orderBy()) :
+                                    filteredData;
+
+                params.total(orderedData.length);
+
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
     });
 });
