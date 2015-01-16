693ed80c5f12c061824669a4b1a79a9f3a04c870
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-01-16 18:04:41 +0100

    Added Migrations

---
+++ src/Integration/Infrastructure/Database/Migrations/M1_Baseline.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentMigrator;
+
+namespace Vertica.Integration.Infrastructure.Database.Migrations
+{
+    [Migration(VersionNumber)]
+	public class M1_Baseline : Migration
+	{
+        public const int VersionNumber = 1;
+
+		public override void Up()
+		{
+			Create.Table(Tables.ErrorLog)
+				.WithColumn("Id").AsInt32().PrimaryKey().Identity()
+				.WithColumn("MachineName").AsString(255)
+                .WithColumn("IdentityName").AsString(255).Nullable()
+                .WithColumn("CommandLine").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Message").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("FormattedMessage").AsCustom("NTEXT").Nullable()
+                .WithColumn("Severity").AsString(50)
+                .WithColumn("Target").AsString(50)
+				.WithColumn("TimeStamp").AsDateTimeOffset();
+
+			Create.Table(Tables.Parameters)
+				.WithColumn("Id").AsInt32().PrimaryKey()
+				.WithColumn("LastMonitorCheck").AsDateTimeOffset();
+
+			Create.Table(Tables.TaskLog)
+				.WithColumn("Id").AsInt32().PrimaryKey().Identity()
+				.WithColumn("Type").AsAnsiString(1)
+				.WithColumn("TaskName").AsString(255)
+				.WithColumn("StepName").AsString(255).Nullable()
+				.WithColumn("Message").AsString(Int32.MaxValue).Nullable()
+				.WithColumn("ExecutionTimeSeconds").AsDouble()
+				.WithColumn("TimeStamp").AsDateTimeOffset()
+				.WithColumn("TaskLog_Id").AsInt32().Nullable()
+				.WithColumn("StepLog_Id").AsInt32().Nullable()
+				.WithColumn("ErrorLog_Id").AsInt32().Nullable();
+		}
+
+		public override void Down()
+		{
+		    throw new NotSupportedException("Migrating down is not supported.");
+		}
+	}
+}
