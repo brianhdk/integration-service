5239a06a81367dc07ffc71c398010695a2451f22
jolyph <johnlypham@gmail.com>
2015-01-16 12:59:02 +0100

    added spa

--- src/Integration.Portal/js/controller/errorController.js
+++ src/Integration.Portal/js/controller/errorController.js
@@ -1,18 +1,24 @@
-﻿var integrationsApp = angular.module('integrationsApp', []);
+﻿var integrationsApp = angular.module('integrationsApp', ['ngRoute']);
+// configure our routes
+integrationsApp.config(function ($routeProvider) {
+	$routeProvider
 
-integrationsApp.controller('ErrorController', function ($scope) {
-	$scope.phones = [
-	  {
-	  	'name': 'Nexus S',
-	  	'snippet': 'Fast just got faster with Nexus S.'
-	  },
-	  {
-	  	'name': 'Motorola XOOM™ with Wi-Fi',
-	  	'snippet': 'The Next, Next Generation tablet.'
-	  },
-	  {
-	  	'name': 'MOTOROLA XOOM™',
-	  	'snippet': 'The Next, Next Generation tablet.'
-	  }
-	];
+		// route for the home page
+		.when('/', {
+			templateUrl: 'pages/profile.html',
+			controller: 'mainController'
+		})
+
+		 // route for the about page
+        .when('/about', {
+            templateUrl: 'pages/about.html',
+            controller: 'aboutController'
+        })
+	;
+});
+integrationsApp.controller('mainController', function ($scope) {
+	$scope.message = 'Look! I am a home page.';
+});
+integrationsApp.controller('aboutController', function ($scope) {
+	$scope.message = 'Look! I am an about page.';
 });
