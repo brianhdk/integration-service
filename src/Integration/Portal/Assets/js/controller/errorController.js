11a2ed08060c3d41851fb12dc0c3c89ef763f98c
jolyph <johnlypham@gmail.com>
2015-01-16 13:32:27 +0100

    fjernet portal

---
+++ src/Integration/Portal/Assets/js/controller/errorController.js
@@ -0,0 +1,27 @@
+﻿var integrationsApp = angular.module('integrationsApp', ['ngRoute']);
+// configure our routes
+integrationsApp.config(function ($routeProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl: '/assets/pages/home.html',
+			controller: 'homeController'
+		})
+
+		 // route for the about page
+        .when('/about', {
+        	templateUrl: '/assets/pages/about.html',
+            controller: 'aboutController'
+        })
+	;
+});
+integrationsApp.controller('mainController', function ($scope) {
+	$scope.message = 'main controller.';
+});
+integrationsApp.controller('homeController', function ($scope) {
+	$scope.message = 'Look! I am a home page.';
+});
+integrationsApp.controller('aboutController', function ($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
