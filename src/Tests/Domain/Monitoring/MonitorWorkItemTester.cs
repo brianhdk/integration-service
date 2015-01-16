8fb2df0e46be97145e20502cef415b4132889006
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-01-16 13:38:32 +0100

    Added tests.

---
+++ src/Tests/Domain/Monitoring/MonitorWorkItemTester.cs
@@ -0,0 +1,46 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using Vertica.Integration.Domain.Monitoring;
+using Vertica.Integration.Infrastructure.Logging;
+using Vertica.Utilities_v4;
+using Vertica.Utilities_v4.Patterns;
+
+namespace Vertica.Integration.Tests.Domain.Monitoring
+{
+	[TestFixture]
+	public class MonitorWorkItemTester
+	{
+		[Test]
+		public void Add_VariousMessagesIncludingIgnoredOnes_VerifyCount()
+		{
+			var ignoreFilter1 = Substitute.For<ISpecification<MonitorEntry>>();
+			var ignoreFilter2 = Substitute.For<ISpecification<MonitorEntry>>();
+
+			ignoreFilter1.IsSatisfiedBy(Arg.Is<MonitorEntry>(entry => entry.Message == "Message to ignore")).Returns(true);
+			ignoreFilter2.IsSatisfiedBy(Arg.Is<MonitorEntry>(entry => entry.Message == "Also to be ignored")).Returns(true);
+
+		    var subject = new MonitorWorkItem(Time.Now, ignoreFilter1, ignoreFilter2);
+            subject.Add(Time.Now, "Source", "Not ignored message", Target.Service);
+            subject.Add(Time.Now, "Source", "Message to ignore", Target.Service);
+            subject.Add(Time.Now, "Source", "Also to be ignored", Target.Service);
+
+		    MonitorEntry[] entries = subject.GetEntries(Target.Service);
+
+            Assert.That(entries.Length, Is.EqualTo(1));
+			Assert.That(entries[0].Message, Is.EqualTo("Not ignored message"));
+		}
+
+		[Test]
+		public void Add_VariousMessagesNoFilters_VerifyCount()
+		{
+		    var subject = new MonitorWorkItem(Time.Now);
+            subject.Add(Time.Now, "Source", "Not ignored message", Target.Service);
+            subject.Add(Time.Now, "Source", "Also not ignored", Target.Service);
+            subject.Add(Time.Now, "Source", "And me neither", Target.Service);
+
+            MonitorEntry[] entries = subject.GetEntries(Target.Service);
+
+			Assert.That(entries.Length, Is.EqualTo(3));
+		}
+	}
+}
