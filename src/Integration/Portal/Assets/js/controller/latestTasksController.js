af1587e1d482c359b1a4390a0a72af3a1c144d3b
jolyph <johnlypham@gmail.com>
2015-01-16 19:17:21 +0100

    added filter

--- src/Integration/Portal/Assets/js/controller/latestTasksController.js
+++ src/Integration/Portal/Assets/js/controller/latestTasksController.js
@@ -1,6 +1,12 @@
 ﻿integrationsApp.controller('latestTasksController', function ($scope, $http) {
 
-    $http.get("/latestTasks").success(function (xhr) {
+	$http.get("/latestTasks?count=10").success(function (xhr) {
         $scope.tasks = xhr;
-    });
+	});
+
+	$scope.getLatestTasks = function (count) {
+		$http.get("/latestTasks?count=" + count).success(function (xhr) {
+			$scope.tasks = xhr;
+		});
+	}
 });
