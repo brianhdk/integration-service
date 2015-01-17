40d9b7ed6af8245137f31588e3d64e2930efa4d2
Henrik Rossen Jakobsen <henrik@rossenjakobsen.dk>
2015-01-17 10:05:33 +0100

    Worked on using ngTable for errors

--- src/Integration/Portal/Assets/js/controller/errorsController.js
+++ src/Integration/Portal/Assets/js/controller/errorsController.js
@@ -1,6 +1,32 @@
-﻿integrationsApp.controller('errorsController', function ($scope, $http) {
+﻿integrationsApp.controller('errorsController', function ($scope, $http, $filter, ngTableParams) {
 
     $http.get("/errors").success(function (xhr) {
-        $scope.errors = xhr;
+        var data = xhr;
+
+        $scope.tableParams = new ngTableParams({
+            page: 1,
+            count: 10,
+            sorting: {
+                //name: 'asc'     // initial sorting
+            },
+            filter: {
+                name: ''       // initial filter
+            }
+        }, {
+            getData: function ($defer, params) {
+                var filteredData = params.filter() ?
+                                   $filter('filter')(data, params.filter()) :
+                                   data;
+
+                var orderedData = params.sorting() ?
+                                    $filter('orderBy')(filteredData, params.orderBy()) :
+                                    filteredData;
+
+                params.total(orderedData.length);
+
+                $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            }
+        });
+
     });
 });
