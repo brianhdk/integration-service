0cd44fe57589a8befeb5f3302fcbf4205006ecdb
jolyph <johnlypham@gmail.com>
2015-01-17 11:12:47 +0100

    added filte to runnings tasks

--- src/Integration/Portal/Assets/js/controller/runningTasksController.js
+++ src/Integration/Portal/Assets/js/controller/runningTasksController.js
@@ -1,6 +1,32 @@
-﻿integrationsApp.controller('runningTasksController', function ($scope, $http) {
+﻿integrationsApp.controller('runningTasksController', function ($scope, $http, $filter, ngTableParams) {
 
-    $http.get("/runningtasks").success(function (xhr) {
-        $scope.tasks = xhr;
-    });
+	$http.get("/runningtasks").success(function (xhr) {
+		var data = xhr;
+
+		$scope.tableParams = new ngTableParams({
+			page: 1,
+			count: 10,
+			sorting: {
+				TimeStamp: 'desc'     // initial sorting
+			},
+			filter: {
+				name: ''       // initial filter
+			}
+		}, {
+			getData: function ($defer, params) {
+				var filteredData = params.filter() ?
+                                   $filter('filter')(data, params.filter()) :
+                                   data;
+
+				var orderedData = params.sorting() ?
+                                    $filter('orderBy')(filteredData, params.orderBy()) :
+                                    filteredData;
+
+				params.total(orderedData.length);
+
+				$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+			}
+		});
+
+	});
 });
