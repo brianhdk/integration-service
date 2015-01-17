9ecf21315a01da3b252e6f2540b2eee8311902ac
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-01-17 14:29:05 +0100

    Azure Blob storage integration.

---
+++ src/Integration.Logging.Kibana/ExportLogsToLogStashWorkItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Vertica.Integration.Logging.Kibana
+{
+    public class ExportLogsToLogStashWorkItem
+    {
+        private readonly Action<Stream, string> _upload;
+
+        public ExportLogsToLogStashWorkItem(Action<Stream, string> upload)
+        {
+            if (upload == null) throw new ArgumentNullException("upload");
+
+            _upload = upload;
+        }
+
+        public void Upload(Stream stream, string name)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            if (String.IsNullOrWhiteSpace(name)) throw new ArgumentException(@"Value cannot be null or empty.");
+
+            _upload(stream, name);
+        }
+    }
+}
---
+++ src/Integration.Logging.Kibana/LoadLogsFromBlobStorageTask.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Vertica.Integration.Logging.Kibana.Infrastructure.Azure;
+using Vertica.Integration.Model;
+
+namespace Vertica.Integration.Logging.Kibana
+{
+    public class LoadLogsFromBlobStorageTask : Task
+    {
+        private readonly IAzureBlobClientFactory _factory;
+
+        public LoadLogsFromBlobStorageTask(IAzureBlobClientFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public override string Description
+        {
+            get { return "Loads Log files from BlobStorage in Azure."; }
+        }
+
+        public override string Schedule
+        {
+            get { return "TBD"; }
+        }
+
+        public override void StartTask(Log log, params string[] arguments)
+        {
+            CloudBlobClient client = _factory.Create();
+
+            CloudBlobContainer container = client.GetContainerReference("logs");
+            container.CreateIfNotExists();
+
+            // todo: configurable
+            var directory = new DirectoryInfo(@"c:\tmp\azure-logs");
+
+            if (!directory.Exists)
+                directory.Create();
+
+            foreach (CloudBlockBlob item in container.ListBlobs().OfType<CloudBlockBlob>())
+            {
+                using (FileStream fileStream = File.OpenWrite(Path.Combine(directory.FullName, item.Name)))
+                {
+                    item.DownloadToStream(fileStream);
+                }
+
+                item.Delete();
+            }
+        }
+    }
+}
