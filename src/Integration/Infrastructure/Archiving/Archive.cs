b5b1f6d9554997fa59fecefd43d215b93940a1f3
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-02-27 11:02:16 +0100

    Archive (undone)

---
+++ src/Integration/Infrastructure/Archiving/Archive.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vertica.Integration.Infrastructure.Archiving
+{
+    public class Archive : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/Tests/Infrastructure/Archiving/ArchiverTester.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Vertica.Integration.Infrastructure.Archiving;
+
+namespace Vertica.Integration.Tests.Infrastructure.Archiving
+{
+    [TestFixture]
+    public class ArchiverTester
+    {
+        [Test]
+        public void Archive()
+        {
+            Archiver subject = Initialize();
+
+            using (Archive archive = subject.New())
+            {
+            }
+        }
+
+        private Archiver Initialize()
+        {
+            return new Archiver();
+        }
+    }
+}
