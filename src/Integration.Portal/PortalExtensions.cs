608f4bad3921f26c7adbb955c44a16933806a1b2
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-02-27 10:35:23 +0100

    First take on moving portal out of core.

---
+++ src/Integration.Portal/PortalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vertica.Integration.Portal
+{
+    public static class PortalExtensions
+    {
+        public static ApplicationConfiguration UsePortal(this ApplicationConfiguration builder, Action<PortalConfiguration> portal = null)
+        {
+            if (builder == null) throw new ArgumentNullException("builder");
+
+            var configuration = new PortalConfiguration();
+
+            if (portal != null)
+                portal(configuration);
+
+            builder.WebApi(x => x.ScanAssembly(typeof (PortalExtensions).Assembly));
+            
+            return builder;
+        }
+    }
+}
