0717601fbcaec20a8a4fc152f42b52d408f27372
jolyph <johnlypham@gmail.com>
2015-02-28 11:39:24 +0100

    Initial work of Configuration

---
+++ src/Integration.Portal/Controllers/ConfigurationController.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Vertica.Integration.Infrastructure.Archiving;
+using Vertica.Integration.Infrastructure.Database.Dapper;
+
+namespace Vertica.Integration.Portal.Controllers
+{
+    public class ConfigurationController : ApiController
+    {
+        private readonly IDapperProvider _dapper;
+
+        public ConfigurationController(IDapperProvider dapper)
+        {
+            _dapper = dapper;
+        }
+
+        public HttpResponseMessage Get()
+        {
+            Archiver archiver = new Archiver(_dapper);
+            SavedArchive[] archives = archiver.GetAll();
+
+            return Request.CreateResponse(HttpStatusCode.OK, archives);
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            Archiver archiver = new Archiver(_dapper);
+            byte[] archive = archiver.Get(id);
+
+            if (archive == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+
+            Stream stream = new MemoryStream(archive);
+            response.Content = new StreamContent(stream);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            {
+                FileName = string.Format("Archive-{0}.zip", id)
+            };
+
+            return response;
+        }
+    }
+}
