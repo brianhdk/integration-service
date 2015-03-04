7233dfbf531001b7ed5852e8f98adb7e8c939639
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-03-04 23:24:16 +0100

    Changes to Portal (including nuget-setup)

--- src/Integration.Portal/Controllers/HomeController.cs
+++ src/Integration.Portal/Controllers/HomeController.cs
@@ -1,24 +1,16 @@
-using System;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Web.Http;
 
 namespace Vertica.Integration.Portal.Controllers
 {
-    public class HomeController : ApiController
+    public class HomeController : Model.Web.HomeController
     {
-        public HttpResponseMessage Get()
+        public override HttpResponseMessage Get()
         {
             if (Request.Headers.Accept.Contains(new MediaTypeWithQualityHeaderValue("text/html")))
-                return Portal.Controllers.AssetsController.ServePortalFile(Request, "Default.html");
+                return AssetsController.ServePortalFile(Request, "Default.html");
 
-            return Request.CreateResponse(HttpStatusCode.OK, "Running.");
-        }
-
-        public HttpResponseMessage Put()
-        {
-            throw new InvalidOperationException("test");
+            return base.Get();
         }
     }
 }
