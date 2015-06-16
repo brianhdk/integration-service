3e22d3f3f91d335914c2d071fa31e362d694447f
Brian Holmgård Kristensen <bhk@yourhost.dk>
2015-06-16 14:48:04 +0200

    Add new projects.

---
+++ src/Integration.UCommerce/Database/UCommerceDb.cs
@@ -0,0 +1,13 @@
+﻿using Vertica.Integration.Infrastructure;
+using Vertica.Integration.Infrastructure.Database;
+
+namespace Vertica.Integration.UCommerce.Database
+{
+    public class UCommerceDb : Connection
+    {
+        public UCommerceDb(ConnectionString connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+}
---
+++ src/Integration.UCommerce/UCommerceIndexMaintenanceStep.cs
@@ -0,0 +1,34 @@
+﻿using Vertica.Integration.Domain.Core;
+using Vertica.Integration.Infrastructure.Database;
+using Vertica.Integration.Model;
+using Vertica.Integration.UCommerce.Database;
+
+namespace Vertica.Integration.UCommerce
+{
+    public class UCommerceIndexMaintenanceStep : Step<MaintenanceWorkItem>
+    {
+        private readonly IDbFactory<UCommerceDb> _uCommerceDb;
+
+        public UCommerceIndexMaintenanceStep(IDbFactory<UCommerceDb> uCommerceDb)
+        {
+            _uCommerceDb = uCommerceDb;
+        }
+
+        public override void Execute(MaintenanceWorkItem workItem, ITaskExecutionContext context)
+        {
+            using (IDbSession session = _uCommerceDb.OpenSession())
+            {
+                session.Execute(@"
+ALTER INDEX [uCommerce_PK_Order] ON [dbo].[uCommerce_PurchaseOrder] REORGANIZE
+ALTER INDEX [IX_Order] ON [dbo].[uCommerce_PurchaseOrder] REORGANIZE
+ALTER INDEX [IX_uCommerce_PurchaseOrder_BasketId] ON [dbo].[uCommerce_PurchaseOrder] REORGANIZE
+");
+            }
+        }
+
+        public override string Description
+        {
+            get { return "Performs index maintenance on certain uCommerce tables."; }
+        }
+    }
+}
