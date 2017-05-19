85b966d7870f9092680176d0aedbac50c79e5a94
brianhdk <bhk@yourhost.dk>
2017-05-19 20:44:43 +0200

    Baseline setting up hangfire and webapi in experiments.slack.

--- src/Experiments.Slack/Program.cs
+++ src/Experiments.Slack/Program.cs
@@ -1,6 +1,12 @@
-﻿using Vertica.Integration;
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+using Vertica.Integration;
 using Vertica.Integration.Domain.LiteServer;
+using Vertica.Integration.Hangfire;
+using Vertica.Integration.Infrastructure;
 using Vertica.Integration.Slack;
+using Vertica.Integration.WebApi;
 
 namespace Experiments.Slack
 {
@@ -9,8 +15,26 @@ namespace Experiments.Slack
 		static void Main(string[] args)
 		{
             using (IApplicationContext context = ApplicationContext.Create(application => application
-                .Database(database => database.IntegrationDb(integrationDb => integrationDb.Disable()))
-                .Logging(logging => logging.Disable())
+                //.Database(database => database.IntegrationDb(integrationDb => integrationDb.Disable()))
+                //.Logging(logging => logging.Disable())
+                .Database(database => database
+                    .IntegrationDb(integrationDb => integrationDb
+                        .PrefixTables("IntegrationService_")))
+                .UseHangfire(hangfire => hangfire
+                    .AddToLiteServer()
+                    .Configuration(configuration => configuration
+                        .UseSqlServerStorage(ConnectionString.FromName("IntegrationDb"), new SqlServerStorageOptions
+                        {
+                            QueuePollInterval = TimeSpan.FromSeconds(15)
+                        })
+                    )
+                )
+                .UseWebApi(webApi => webApi
+                    .AddToLiteServer()
+                    .HttpServer(httpServer => httpServer.Configure(configure =>
+                    {
+                        configure.App.UseHangfireDashboard("/hangfire");
+                    })))
                 .UseLiteServer(liteServer => liteServer
                     .AddFromAssemblyOfThis<Program>())
                 //.UseSlack(slack => slack
