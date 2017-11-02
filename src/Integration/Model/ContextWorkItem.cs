50d056dec1157f8f20c6849c702222cbd964403b
brianhdk <bhk@yourhost.dk>
2017-11-02 20:43:36 +0100

    upgrade 3rd party + bump versions

---
+++ src/Integration/Model/ContextWorkItem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace Vertica.Integration.Model
+{
+    [Obsolete("Replaced by functionality directly on ITaskExecutionContext")]
+    public abstract class ContextWorkItem
+    {
+        private readonly IDictionary<string, object> _context;
+
+        protected ContextWorkItem()
+        {
+            _context = new Dictionary<string, object>();
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException(@"Value cannot be null or empty.", nameof(name));
+
+                object value;
+                _context.TryGetValue(name, out value);
+                return value;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException(@"Value cannot be null or empty.", nameof(name));
+
+                _context[name] = value;
+            }
+        }
+
+        public T Context<T>(string name, T context = null) where T : class
+        {
+            if (context != null)
+            {
+                this[name] = context;
+                return context;
+            }
+
+            return this[name] as T;
+        }
+    }
+}
