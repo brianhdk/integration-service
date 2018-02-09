bbf6af41ed2598a15d8b4c46473f81e090d24d33
brianhdk <bhk@yourhost.dk>
2018-02-09 14:40:13 +0100

    serilog upgrade

--- src/Integration.Serilog/Properties/AssemblyInfo.cs
+++ src/Integration.Serilog/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha1")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha2")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
